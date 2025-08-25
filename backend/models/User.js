33c8bb0507a8b6659cd893ffaaa14ca393f6f28e
Jrx <jrxdeveloppement@gmail.com>
2025-08-25 18:41:53 +0200

    backend/frontend/1

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' }
 });
 
 module.exports = mongoose.model('User', userSchema, 'User');
